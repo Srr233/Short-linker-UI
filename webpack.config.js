967c962cd0ba082049a2d61f254be716cfaf1cbb
Siarhei Saltanovich <siarhei_saltanovich@epam.com>
2021-11-02 15:25:40 +0300

    settings main: fix problem with reload the page in a dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -14,7 +15,12 @@ module.exports = {
         filename: 'index.js'
     },
     plugins: [
-        new HtmlWebpackPlugin({template: './src/index.html'})
+        new HtmlWebpackPlugin({template: './src/index.html'}),
+        new CopyWebPlugin({
+            patterns: [
+                { from: "./src/style.css", to: "./" }
+            ]
+        })
     ],
     mode: 'production'
 }
