2873d01effea5e607b849860e7e99dbf01d35d06
Siarhei Saltanovich <siarhei_saltanovich@epam.com>
2021-12-01 10:33:20 +0300

    main v1: change origin

--- src/generic data/origin.js
+++ src/generic data/origin.js
@@ -1,7 +1,9 @@
 const auth_service_URL = "https://short-linker-auth.herokuapp.com";
 const short_linker = "https://short-linker-main.herokuapp.com";
+const short_linker_engine = "https://short-linker-engine.herokuapp.com";
 
 export {
     auth_service_URL,
-    short_linker
+    short_linker,
+    short_linker_engine
 }
--- src/modules/addNewLinkDOM.js
+++ src/modules/addNewLinkDOM.js
@@ -1,28 +1,28 @@
+import { short_linker_engine } from "../generic data/origin";
 import { show_statistic } from "./show_statistic";
 
 const listOfLinks = document.querySelector('.listOfLinks');
 
-const remote = 'http://localhost:3000';
-
 const add_link = (short_link) => {
-    const fullLink = remote + '/' + short_link;
+    const fullLink = short_linker_engine + '/' + short_link;
     const div_link_wrapper = document.createElement('div');
-    const span_link = document.createElement('span');
+    const a_link = document.createElement('a');
     const div = document.createElement('div');
     const button_statistic = document.createElement('button');
     const button_data_delete = document.createElement('button');
 
     div_link_wrapper.classList.add('link_wrapper');
-    span_link.classList.add('link');
+    a_link.classList.add('link');
     button_statistic.addEventListener('click', show_statistic);
     button_data_delete.dataset.delete = short_link;
 
-    div_link_wrapper.insertAdjacentElement('beforeend', span_link);
+    div_link_wrapper.insertAdjacentElement('beforeend', a_link);
     div_link_wrapper.insertAdjacentElement('beforeend', div);
     div.insertAdjacentElement('beforeend', button_statistic);
     div.insertAdjacentElement('beforeend', button_data_delete);
 
-    span_link.textContent = fullLink;
+    a_link.textContent = fullLink;
+    a_link.href = fullLink;
     button_statistic.textContent = 'statistic';
     button_data_delete.textContent = 'delete';
 
